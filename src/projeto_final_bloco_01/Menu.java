ae010fcb5fd9dbf2a2f8a6c465ab4570ca876132
Wendy Souza <contato.wendy@hotmail.com>
2024-06-20 17:40:44 -0300

    Implementação do Controller e Métodos dos CRUD

--- src/projeto_final_bloco_01/Menu.java
+++ src/projeto_final_bloco_01/Menu.java
@@ -1,18 +1,42 @@
 package projeto_final_bloco_01;
 
+import java.io.IOException;
 import java.util.*;
+
 import projeto_final_bloco_01.*;
+import projeto_final_bloco_01.controller.ProdutoController;
 import projeto_final_bloco_01.model.*;
 
 public class Menu {
 
 	public static void main(String[] args) {
 
+		ProdutoController produto = new ProdutoController();
+
 		Scanner leia = new Scanner(System.in);
 
-		int opcao;
+		int opcao, codigo, tipo, quantidade;
+		String nome;
+		float preco, compraMinima;
+		
+		System.out.println("\nAuthentic Beauty");
+		
+		Maquiagens m1 = new Maquiagens(produto.gerarCodigo(), 5, 1, "Base", 15.0f, 20.0f);
+		produto.cadastrar(m1);
+
+		Maquiagens m2 = new Maquiagens(produto.gerarCodigo(), 80, 1, "Corretivo", 20.0f, 20.0f);
+		produto.cadastrar(m2);
+
+		Perfumaria p1 = new Perfumaria(produto.gerarCodigo(), 10, 2, "Suave", 80.0f, 100.0f);
+		produto.cadastrar(p1);
+
+		Perfumaria p2 = new Perfumaria(produto.gerarCodigo(), 1, 2, "Marcante", 100.0f, 100.0f);
+		produto.cadastrar(p2);
+
+		produto.listarTodos();
 
 		while (true) {
+			System.out.println("                                                     ");
 			System.out.println("*****************************************************");
 			System.out.println("                                                     ");
 			System.out.println("                   Authentic Beauty                  ");
@@ -22,7 +46,7 @@ public class Menu {
 			System.out.println("            1 - Cadastrar produto                    ");
 			System.out.println("            2 - Atualizar cadastro de produto        ");
 			System.out.println("            3 - Listar produtos                      ");
-			System.out.println("            4 - Buscar produtos por código           ");
+			System.out.println("            4 - Buscar produto por código            ");
 			System.out.println("            5 - Excluir produto                      ");
 			System.out.println("            6 - Sair                                 ");
 			System.out.println("                                                     ");
@@ -30,16 +54,14 @@ public class Menu {
 			System.out.println("Entre com a opção desejada:                          ");
 			System.out.println("                                                     ");
 
-			opcao = leia.nextInt();
-			
-			Produto p1 = new Produto(3515, 1, 2, "Perfumes", 15.0f);
-			p1.visualizar();
-			
-			Maquiagens m1 = new Maquiagens(5428, 25, 1, "Base Facial", 15.0f, 50.0f );
-			m1.visualizar();
-			
-			Perfumaria pp1 = new Perfumaria (5214, 5, 2, "Perfume Feminino", 80.0f, 100.0f);
-			pp1.visualizar();			
+			try {
+				opcao = leia.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("\nDigite somente valores inteiros!");
+				leia.nextLine();
+				opcao = 0;
+				continue;
+			}
 
 			if (opcao == 6) {
 				System.out.println("\nAgradecemos pela visita à Authentic Beauty!");
@@ -47,30 +69,120 @@ public class Menu {
 				leia.close();
 				System.exit(0);
 			}
-			
+
+
 			switch (opcao) {
 			case 1:
+				leia.nextLine();
 				System.out.println("Cadastrar produto\n\n");
+				System.out.println("Digite o nome do produto: ");
+				nome = leia.nextLine();
+				System.out.println("Digite a quantidade do produto: ");
+				quantidade = leia.nextInt();
+				System.out.println("Digite o preço do produto R$: ");
+				preco = leia.nextFloat();
+				
+				do {
+					System.out.println("Digite a categoria do produto (1-Maquiagens ou 2-Perfumaria): ");
+					tipo = leia.nextInt();
+				} while (tipo < 1 && tipo > 2);
+				
+				leia.nextLine();
 
+					switch (tipo) {
+					case 1 -> {
+					System.out.println("Digite o valor mínimo de compra (R$): ");
+					compraMinima = leia.nextFloat();
+					produto.cadastrar(
+							new Maquiagens(produto.gerarCodigo(), quantidade, tipo, nome, preco, compraMinima));
+					}
+
+					case 2 -> {
+					System.out.println("Digite o valor mínimo de compra (R$): ");
+					compraMinima = leia.nextInt();
+					produto.cadastrar(
+							new Maquiagens(produto.gerarCodigo(), quantidade, tipo, nome, preco, compraMinima));
+
+					}
+					}
+
+				keyPress();
 				break;
 			case 2:
 				System.out.println("Atualizar cadastro de produto\n\n");
+				
+				System.out.println("Digite o código do produto que deseja atualizar: ");
+			    codigo = leia.nextInt();
 
+			    var buscaProduto = produto.buscarNaCollection(codigo);
+
+			    if (buscaProduto != null) {
+			        leia.nextLine(); 
+
+			        System.out.println("Digite o nome do produto: ");
+			        nome = leia.nextLine();
+			        System.out.println("Digite a quantidade do produto: ");
+			        quantidade = leia.nextInt();
+			        System.out.println("Digite o preço do produto R$: ");
+			        preco = leia.nextFloat();
+
+			        do {
+			            System.out.println("Digite a categoria do produto (1-Maquiagens ou 2-Perfumaria): ");
+			            tipo = leia.nextInt();
+			        } while (tipo < 1 || tipo > 2);
+
+			        switch (tipo) {
+			            case 1:
+			                System.out.println("Digite o valor mínimo de compra (R$): ");
+			                compraMinima = leia.nextFloat();
+			                produto.atualizar(new Maquiagens(codigo, quantidade, tipo, nome, preco, compraMinima));
+			                break;
+
+			            case 2:
+			                System.out.println("Digite o valor mínimo de compra (R$): ");
+			                compraMinima = leia.nextFloat();
+			                produto.atualizar(new Perfumaria(codigo, quantidade, tipo, nome, preco, compraMinima));
+			                break;
+
+			            default:
+			                System.out.println("Opção inválida!");
+			                break;
+			        }
+
+			        System.out.println("Produto atualizado com sucesso!");
+
+			    } else {
+			        System.out.println("Produto não encontrado!");
+			    }
+				
+				keyPress();
 				break;
 			case 3:
-				System.out.println("Listar produtos\n\n");
-
+				System.out.println("Listar produto\n\n");
+				produto.listarTodos();
+				keyPress();
 				break;
 			case 4:
 				System.out.println("Buscar produto por código\n\n");
+				System.out.println("Digite o código do produto: ");
+				codigo = leia.nextInt();
 
-				break;	
+				produto.procurarPorCodigo(codigo);
+				
+				keyPress();
+				break;
 			case 5:
 				System.out.println("Excluir produto\n\n");
+			    System.out.println("Digite o código do produto que deseja excluir: ");
+			    codigo = leia.nextInt();
 
+			    produto.deletar(codigo);
+				keyPress();
 				break;
 			default:
 				System.out.println("\nOpção Inválida!\n");
+
+				keyPress();
 				break;
 			}
 
@@ -81,8 +193,22 @@ public class Menu {
 		System.out.println("\n*********************************************************\n");
 		System.out.println("Authentic Beauty - Sua autenticidade, sua beleza!");
 		System.out.println("Projeto desenvolvido por: ");
-		System.out.println("Wendy Souza - contato.wendy@hotmail.com");		
+		System.out.println("Wendy Souza - contato.wendy@hotmail.com");
 		System.out.println("GitHub: github.com/WendySouza");
 		System.out.println("\n*********************************************************\n");
+	}
+
+	public static void keyPress() {
+
+		try {
+
+			System.out.println("\n\nPressione Enter para Continuar...");
+			System.in.read();
+
+		} catch (IOException e) {
+
+			System.out.println("**Você pressionou uma tecla diferente de enter!!!**");
+
+		}
 	}
 }
--- src/projeto_final_bloco_01/repository/ProdutoRepository.java
+++ src/projeto_final_bloco_01/repository/ProdutoRepository.java
@@ -4,8 +4,8 @@ import projeto_final_bloco_01.model.*;
 
 public interface ProdutoRepository {
 	
-	public void cadastrar(Produto conta);
-	public void atualizar(Produto conta);
+	public void cadastrar(Produto produto);
+	public void atualizar(Produto produto);
 	public void listarTodos();
 	public void procurarPorCodigo(int codigo);
 	public void deletar(int codigo);
